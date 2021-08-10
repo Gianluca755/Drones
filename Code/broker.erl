5851bb2b2c106e9aad63bb481127b72e3532022c
me <ermacora.gianluca25@gmail.com>
2021-08-10 09:24:05 +0200

    new broker style

--- Code/broker.erl
+++ Code/broker.erl
@@ -13,7 +13,8 @@
 
 % Key and value of an Order
 % {ClentID, OrderID} {Source, Destination, Weight, Status}
-% Status ::= awaitBck, awaitManager, inProgress, inDelivery, delivered.
+% Status ::= awaitBck, awaitManager, inProgressAwaitBck, inProgress, inDeliveryAwaitBck, inDelivery, delivered.
+% bck Status ::=     saved,      awaitManager,      inProgress,                   inDelivery,             delivered
 
 % normal init
 start(PrimaryManagerAddr, BckManagerAddr) ->
@@ -59,72 +60,6 @@ startBck(Primary, PrimaryManagerAddr, BckManagerAddr) ->
 .
 
 
-loopPrimary(OrderTable, AddrRecord) ->
-    % the ping msg has higher priority
-    % the primary server respong immediately, while the backup has some delay to mantain 5 pckt/second
-    receive
-        {Sender, ping} when Sender == AddrRecord#addr.bckBrokerAddr ->
-            AddrRecord#addr.bckBrokerAddr ! {self(), pingResponse}
-    after
-        0 -> true
-    end,
-
-    receive
-    % normal order
-    Msg = { makeOrder, ClientID, OrderID, Source, Destination, Weight } ->
-        ets:insert(OrderTable, {Source, Destination, Weight, awaitBck}),
-        AddrRecord#addr.bckBrokerAddr ! Msg,
-        loopPrimary(OrderTable, AddrRecord) ;
-
-    % confirmation from bck broker
-    { confirmBck, ClientID, OrderID } ->
-        ets:updateTable(OrderTable, {ClientID, OrderID}, awaitManager),
-        {Source, Destination, Weight, Status} = ets:lookup({ClientID, OrderID}),
-        AddrRecord#addr.primaryManagerAddr ! {makeOrder, ClientID, OrderID, Source, Destination, Weight },
-        loopPrimary(OrderTable, AddrRecord) ;
-
-    % confirmation from manager
-    Msg = { confirmManager, ClientID, OrderID} ->
-        ets:updateTable(OrderTable, {ClientID, OrderID}, inProgress),
-        AddrRecord#addr.bckBrokerAddr ! Msg,
-        loopPrimary(OrderTable, AddrRecord) ;
-
-    Msg = { droneElected, ClientID, OrderID} ->
-        ets:updateTable(OrderTable, {ClientID, OrderID}, inDelivery),
-        AddrRecord#addr.bckBrokerAddr ! Msg,
-        loopPrimary(OrderTable, AddrRecord) ;
-
-    % query
-    { Pid, queryOrder, ClientID, OrderID } ->
-        {_, _, _, Status} = ets:lookup(OrderTable, {ClentID, OrderID}),
-        Pid ! Status,
-        loopPrimary(OrderTable, AddrRecord) ;
-
-    Other -> io:format("Message not expected: ~w~n", [Other])
-    end
-.
-
-
-loopBackup(OrderTable, AddrRecord, LastPingTime) ->
-    Time = erlang:system_time(milli_seconds),
-    if
-        Time - LastPingTime < 2000  ->
-            receive
-                {Sender, pingResponse} when Sender == AddrRecord#addr.primaryBrokerAddr ->
-                    % if the primary responded
-
-                    sendPingLater(self(), AddrRecord#addr.primaryBrokerAddr), % send ping after 200 ms
-                    CurrentPingTime = 200 + erlang:system_time(milli_seconds),
-
-                    loopBackup(OrderTable, AddrRecord, CurrentPingTime)
-
-                %Other -> true
-            end;
-
-        true -> io:format("Primary broker not responding: ~w~n", [self()]) % primary not responding
-    end
-.
-
 
 sendPingLater(From, To) ->
     timer:sleep(200),         % wait 200 ms
@@ -133,7 +68,7 @@ sendPingLater(From, To) ->
 
 %%% utils %%%
 
-updateTable(Table, Key, NewStatus) ->
+updateTableStatus(Table, Key, NewStatus) ->
     {Source, Destination, Weight, Status} = ets:lookup(Table, Key),
     Result = ets:insert(Table, {Key, {Source, Destination, Weight, NewStatus} } ), % overwrite
     if
@@ -142,27 +77,3 @@ updateTable(Table, Key, NewStatus) ->
     end
 .
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%primaryPinger(AddrRecord) ->
-%    receive
-%        {AddrRecord#addr.bckPingerAddr, ping} ->
-%            AddrRecord#addr.bckPingerAddr ! {self(), ok},
-%            primaryPinger(AddrRecord)
-%    after
-%        2000 -> io:format("Backup broker not responding: ~w~n", [self()])
-%    end
-%.
-
-%bckPinger(AddrRecord, LastPingTime) ->
-%    receive
-%        {AddrRecord#addr.primaryPinger, ok} ->
-%            sleep(200), % wait 200 ms
-%            AddrRecord#addr.primaryBrokerAddr ! ping,
-%            CurrentPingTime = erlang:system_time(milli_seconds),
-%            bckPinger(AddrRecord, CurrentPingTime)
-%    after
-%        2000 -> io:format("Primary broker not responding: ~w~n", [self()]),
-%                AddrRecord#adrr.bckBrokerAddr ! {self(), primaryDown}
-%    end
-%.
