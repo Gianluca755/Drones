0f7ef23ebbd951863ec79a474e5aa0bd52dc75b8
me <ermacora.gianluca25@gmail.com>
2021-08-11 08:32:30 +0200

    new broker server

--- Code/manager.erl
+++ Code/manager.erl
@@ -1,2 +1,43 @@
 -module(manager).
 
+-export([start/0]).
+
+
+
+start() ->
+
+    Primary = spawn(manager, startPrimary, []),
+    Bck = spawn(manager, startBck, [Primary]),
+
+    io:format("Primary manager: ~w~n", [Primary]),
+    io:format("Backup manager: ~w~n", [Bck])
+.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
