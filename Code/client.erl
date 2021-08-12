447c994407ed5bb76d2e2c752590a3c01f321188
unknown <andrea.rovere96@gmail.com>
2021-08-12 10:37:32 +0200

    removed round and added development plan

--- Code/client.erl
+++ Code/client.erl
@@ -9,7 +9,7 @@
 
 %sends a delivery requests, randomizes the X and Y coordinates of the delivery location(in 1-100 range) and the weight of the package(in 1-1000 range)
 delivery_Request(ClientID, OrderID, Broker_Server_Addr, PosA_X, PosA_Y) ->
-	Broker_Server_Addr ! {delivery_Request, self(), ClientID, OrderID,  {{PosA_X, PosA_Y}, {round(rand:uniform(100)), round(rand:uniform(100))}, round(rand:uniform(1000))}}.
+	Broker_Server_Addr ! {delivery_Request, self(), ClientID, OrderID,  {{PosA_X, PosA_Y}, {rand:uniform(100), rand:uniform(100)}, rand:uniform(1000)}}.
 
 
 status_Request(Broker_Server_Addr, ClientID, OrderID)->
