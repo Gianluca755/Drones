2b4b99d5abdf8526e07c709af12c592d8d42fba8
unknown <andrea.rovere96@gmail.com>
2021-08-16 10:50:29 +0200

    fixed errors in drones

--- Code/drone.erl
+++ Code/drone.erl
@@ -7,26 +7,26 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([join_Request/2]).
+-export([drone_Loop_init/3]).
+
+
 drone_Loop_init(Manager_Server_Addr, DroneID, NeighbourList)->
 	join_Request(Manager_Server_Addr,DroneID),
-drone_Loop(Manager_Server_Addr, DroneID, NeighbourList)
+	drone_Loop(Manager_Server_Addr, DroneID, NeighbourList)
 .
 
-drone_Loop(Manager_Server_Addr, DroneID, NeighbourList)->
-	
+drone_Loop(Manager_Server_Addr, DroneID, NeighbourList) ->
 	
 	receive
-		{dronesList, Manager_Server_Addr, DronesList}->
-			connect_to_drones(DronesList),
-		{ackJoin, DroneAddr, NeighbourDroneID}->
-			drone_Loop(Manager_Server_Addr, DroneID, NeighbourList ++ NeighbourDroneID),
-		{droneStatus, Manager_Server_Addr}->
+		{dronesList, Manager_Server_Addr, DronesList} ->
+			connect_to_drones(DronesList, DroneID);
+		{connection, DroneAddr, NeighbourDroneID} ->
+			drone_Loop(Manager_Server_Addr, DroneID, NeighbourList ++ NeighbourDroneID);
+		{droneStatus, Manager_Server_Addr} ->
 			Manager_Server_Addr ! {droneStatus, self(), DroneID}
-	end
-	
-	drone_Loop(Manager_Server_Addr, DroneID, NeighbourTable)
-	.
+	end,
+	drone_Loop(Manager_Server_Addr, DroneID, NeighbourList)
+.
 
 join_Request(Manager_Server_Addr, DroneID) ->
 	Manager_Server_Addr ! {joinRequest, self(), DroneID, {rand:uniform(1500)},{}}
@@ -34,20 +34,20 @@ join_Request(Manager_Server_Addr, DroneID) ->
 
 
 
-	Manager_Server_Addr ! {inDelivery, self(), ClientID, OrderID, {}},
-	Manager_Server_Addr ! {delivered, {}, ClientID, OrderID, {}}
+	%Manager_Server_Addr ! {inDelivery, self(), ClientID, OrderID, {}},
+	%Manager_Server_Addr ! {delivered, {}, ClientID, OrderID, {}}
 
 
-	Drone_Addr ! {election, self(), ClientID, OrderID, {Source, Destination, Weight}}
+	%Drone_Addr ! {election, self(), ClientID, OrderID, {Source, Destination, Weight}}
 
 
 
-connect_to_drones(DronesList)->
+connect_to_drones(DronesList, DroneID)->
 	
 	if
 		length(DronesList) >0 ->
 			hd(DronesList) ! {connection, self(), DroneID},
-			connect_to_drones(tl(DronesList))
+			connect_to_drones(tl(DronesList), DroneID)
 	end
 .
 
